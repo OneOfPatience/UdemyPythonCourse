bee0abc500b1c556edeca727627959116907a501
J.Rashad <thomas.jeff@live.com>
2024-04-16 23:33:53 -0500

    Added course notes

---
+++ day1-variables.py
@@ -0,0 +1,14 @@
+#name = input("What is your name?")
+#print(name)
+
+#name = "Jack"
+#print(name)
+
+#name = "Larry"
+#print(name)
+
+#print(len(input("What is your name?")))
+#This can be broken down into seperate lines of code
+name = input("What is your name?\n")
+length = len(name)
+print(length)
