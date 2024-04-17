93bc3d9f15d4e5fb6b7694da1943d92f761eacb0
J.Rashad <thomas.jeff@live.com>
2024-04-16 23:32:31 -0500

    Added course notes clear ls
    clear

---
+++ day1-printing.py
@@ -0,0 +1,6 @@
+#Write your code below this line 👇
+print("Hello World!")
+print("Welcome to the Band Name Generator.")
+city = input("What city where you born in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
