42f5f5b50eb6114a08e631bd6f975bb29d5bc236
J.Rashad <thomas.jeff@live.com>
2024-04-28 22:09:17 -0500

    Changed Comment Header

--- day1-printing.py
+++ day1-printing.py
@@ -1,6 +1,6 @@
-#Write your code below this line 👇
-print("Hello World!")
-print("Welcome to the Band Name Generator.")
-city = input("What city where you born in?\n")
-pet = input("What is your pet's name?\n")
-print("Your band name could be " + city + " " + pet)
+#Printing To The Screen
+#print("Hello World!")
+#print("Welcome to the Band Name Generator.")
+#city = input("What city where you born in?\n")
+#pet = input("What is your pet's name?\n")
+#print("Your band name could be " + city + " " + pet)
