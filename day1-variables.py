2a8300ab97849525e4593f74796a74caf5ec1335
J.Rashad <thomas.jeff@live.com>
2024-04-28 21:56:39 -0500

    Added Comments

--- day1-variables.py
+++ day1-variables.py
@@ -1,14 +1,20 @@
+#Printing Values to the Screen
+
+#1 Printing user input stored in variable
 #name = input("What is your name?")
 #print(name)
 
+#2 Print the contents of a variable
 #name = "Jack"
 #print(name)
 
+#3 Print the contents of a variable after it was reassigned 
 #name = "Larry"
 #print(name)
 
+#4 Print the value of a function applied to user input
 #print(len(input("What is your name?")))
 #This can be broken down into seperate lines of code
-name = input("What is your name?\n")
-length = len(name)
-print(length)
+#name = input("What is your name?\n")
+#length = len(name)
+#print(length)
